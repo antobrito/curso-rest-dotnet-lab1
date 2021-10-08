8f1ba8f04c5dc8c299432a0c377950447e4343e5
Esteban Solano <stvansolano>
2021-10-07 19:27:44 -0600

    Remover comentario

--- src/ConsoleApp/ConsoleApp/Program.cs
+++ src/ConsoleApp/ConsoleApp/Program.cs
@@ -7,10 +7,6 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
-            // Repo (github, nube, etc)
-            // Staging (.csproj, .sln, Program.cs)
-            // Mi local 
         }
     }
 }
